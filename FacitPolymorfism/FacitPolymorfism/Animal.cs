8b70f9d8e940e1b1c558c7fe450ec7b6b24d1525
Camdor <kt.keijser@gmail.com>
2015-04-10 10:22:12 +0200

    FacitPolymorfDone

---
+++ FacitPolymorfism/FacitPolymorfism/Animal.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FacitPolymorfism
+{
+    /// <summary>
+    /// Super class for all Animals
+    /// </summary>
+    public class Animal //Step1 was to create this class
+    {
+        #region Step2
+        public string Name { get; set; }
+        public string Origin { get; set;}
+        public int Age { get; set; }
+        public bool IsMammal { get; set; }
+        #endregion
+
+    }
+    #region Step3
+    public class Horse : Animal
+    {
+        #region Step4Horse
+        public string breed {get; set;}
+        public bool IsRaceHorse {get;set; }
+        #endregion
+    }
+    public class Dog : Animal
+    {
+        #region Step4Dog
+        public bool HasTail {get;set;}
+        public string TypeOfDog {get;set;}
+#endregion
+    }
+    public class Worm : Animal
+    {
+        #region Step4Worm
+        public bool IsBandWorm {get;set;}
+        public int length {get;set;}
+#endregion
+
+    }
+    public class Hedgehog : Animal
+    {
+        #region Step4Hedgehog
+        public int NumOfSpikes{get;set;}
+        public bool IsResting {get; set;}
+        #endregion
+    }
+    public class Bird : Animal
+    {
+        #region Step4Bird
+        public bool IsMovingBird {get;set;}
+        public int BeakSize {get;set;}
+        #endregion
+    }
+    #endregion
+#region Step11
+    /// <summary>
+    /// The flamingo class, holds properties specific for flamingos. Subclass of bird
+    /// </summary>
+    public class Flamingo : Bird
+    {
+        #region Step12 Falmingo
+        public bool standing { get; set; }
+        public int numOfChildren { get; set; }
+        #endregion
+    }
+
+    /// <summary>
+    /// The Pelican class, holds properties specific for pelican. Subclass of bird
+    /// </summary>
+    public class Pelican : Bird
+    {
+        #region Step12 Pelican
+        public int sizeOfMouth { get; set; }
+        public string name { get; set; }
+        #endregion
+    }
+
+    /// <summary>
+    /// The pelican class, holds properties specific for pelican. Subclass of bird
+    /// </summary>
+    public class Swan : Bird
+    {
+        #region Step12 Falmingo
+        public bool isWhite { get; set; }
+        public int neckLength { get; set; }
+        #endregion
+    }
+#endregion
+}
---
+++ FacitPolymorfism/FacitPolymorfism/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FacitPolymorfism
+{
+    class Program
+    {
+        /// <summary>
+        /// The main class, will keep the program open and run some operations.
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args) 
+        {
+            //For step 1, see Animal.cs
+            #region Step5
+            List<Animal> animalList = new List<Animal>();
+            #endregion
+            #region Step6
+            animalList.Add(new Dog { Name = "Fido", Age = 2, HasTail = true, Origin = "Sweden", IsMammal = true, TypeOfDog = "Wolfdog" });
+            animalList.Add(new Worm { Name = "Ester", Age = 1, IsBandWorm = true, IsMammal = false, length = 13, Origin = "Kenya" });
+            animalList.Add(new Horse { Name = "Spirit", Age = 2, breed = "Shetland", IsMammal = true, IsRaceHorse = true, Origin = "Iceland" });
+            #endregion
+            #region Step7
+            foreach (Animal ani in animalList)
+            {
+                Console.WriteLine(Convert.ToString(ani.GetType()).Split('.').Last());
+            }
+            Console.ReadLine();
+            #endregion
+            #region Step8-10
+            List<Dog> doges = new List<Dog>(); //Step 8
+            //doges.Add(new Horse()); Step 9 <--- Doesn't work because Horse is not TypeOf Dog Step 10
+            #endregion
+            //For step 11 and 12, see Animal.cs
+            #region Step 13-16
+            //Step 13: Bird (List<Bird>)
+            //Step 14: Animal (List<Animal>)
+            //Step 15: Bird
+            //Step 16: Animal
+            #endregion
+        }
+    }
+}
